4c0304c1d08262e20c5f5d07c62046845addb2da
Edgar <ragdemartinez.18@gmail.com>
2020-01-24 00:47:53 -0600

    Creacion conexion y primera tabla

---
+++ php/conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class Conexion{
+    private $usuario="root";
+    private $base="erp";
+    private $password="";
+    private $host="localhsot";
+    protected $sentencia ="";
+    public $conexion;
+}
+
+
+
+
+?>
