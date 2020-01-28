c622963db3f4baa25bb3fed8c777205a5792cd57
Edgar <ragdemartinez.18@gmail.com>
2020-01-27 18:51:21 -0600

    Conexion y metodos

--- php/conexion.php
+++ php/conexion.php
@@ -1,16 +1,30 @@
-<?php
+<?php 
+	class Conexion{
+		private $usuario "root";
+		private $base "erp";
+		private $password="";
+		private $host="localhost";
+		protected $sentencia="";
+		public $conexion;
 
+		private function abrirConexion(){
+			$this->conexion = new mysqli($this->host, $this->usuario, $this->password, $this->base);
+		}
+		private function cerrarConexion(){
+			$this->conexion->close();
+		}
+		
+		public function ejecutarSentencia(){
+			$this->abrirConexion();
+			$this->conexion->query($this->sentencia);
+			$this->cerrarConexion();
+		}
 
-class Conexion{
-    private $usuario="root";
-    private $base="erp";
-    private $password="";
-    private $host="localhsot";
-    protected $sentencia ="";
-    public $conexion;
-}
-
-
-
+		public function obtenerSentencia(){
+			$this->abrirConexion();
+			$resultado = $this->conexion->query($this->sentencia);
+			return $resultado;
+		}
+	} 
 
 ?>
